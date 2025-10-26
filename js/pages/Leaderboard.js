872697a7da8bf58257e3da00a226179dd40c34ba
fayodayo <165217859+fayodayo@users.noreply.github.com>
2025-10-26 21:49:21 +0000

    Update Leaderboard.js

--- js/pages/Leaderboard.js
+++ js/pages/Leaderboard.js
@@ -55,7 +55,7 @@ export default {
                                     <a class="type-label-lg" target="_blank" :href="score.link">{{ score.level }}</a>
                                 </td>
                                 <td class="score">
-                                    <p>+{{ localize(score.score)}}</p>
+                                    <p>+{{score.score}}</p>
                                 </td>
                             </tr>
                         </table>
