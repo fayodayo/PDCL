8526e261d3188d6021e3c760f11a1d2f378f7182
JoaqFirr <JoaqFirr@gmail.com>
2026-07-28 13:12:06 -0300

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -34,8 +34,8 @@ export default {
                                     class="type-label-lg"
                                     :class="{
                                         'gradient-1': i === 0,
-                                        'gradient-2': i === 4,
-                                        'gradient-3': i === 1,
+                                        'gradient-2': i === 1,
+                                        'gradient-3': i === 2,
                                         'gradient-4': i === 3,
                                         'gradient-5': i === 4
                                     }">
