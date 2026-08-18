d82f69f24fddc782e00ed618ed1615d4f4860cbd
JoaqFirr <JoaqFirr@gmail.com>
2026-08-18 12:27:54 -0300

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -35,8 +35,8 @@ export default {
                                     :class="{
                                         'gradient-1': i === 0,
                                         'gradient-2': i === 1,
-                                        'gradient-3': i === 3,
-                                        'gradient-4': i === 2,
+                                        'gradient-3': i === 2,
+                                        'gradient-4': i === 3,
                                         'gradient-5': i === 4
                                     }">
                                 {{ level?.name || \`Error (\${err}.json)\` }}
